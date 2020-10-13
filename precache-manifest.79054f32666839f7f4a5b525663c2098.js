1e7385e7b405930b991aa24b6c482bd675f482e7
Giovanna <ggarciabas@gmail.com>
2020-10-13 16:46:31 -0300

    feat: versão 1

---
+++ precache-manifest.79054f32666839f7f4a5b525663c2098.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc72362e6d505450806b0b9a42b1fbec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea25901cf837dc258cfb",
+    "url": "/static/css/main.41f573a2.chunk.css"
+  },
+  {
+    "revision": "6cfcdd146177082e0cc7",
+    "url": "/static/js/2.a4eda2df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a4eda2df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea25901cf837dc258cfb",
+    "url": "/static/js/main.ac0e21f6.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/static/media/landing.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/static/media/logo.1653c647.svg"
+  }
+]);
